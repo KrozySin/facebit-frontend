c37918d7eb3b4927fe4b3b3ce8fef7025e8c4f4f
V <narutoninja342@gmail.com>
2024-06-12 23:20:44 +0800

    Update: sign in & up

--- src/pages/signin/index.tsx
+++ src/pages/signin/index.tsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useState } from "react";
 import { Button, Form } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const SignIn = () => {
   const [username, setUsername] = useState("");
@@ -23,33 +24,38 @@ const SignIn = () => {
   };
 
   return (
-    <div className="d-flex justify-content-center align-items-center w-100">
+    <div className="d-flex justify-content-center align-items-center w-100 auth-panel">
       <div className="sign-up">
-        <Form.Label htmlFor="su_username">User Name</Form.Label>
+        <h1 className="__auth-title">Sign In</h1>
+
+        <hr />
+
+        <Form.Label htmlFor="su_username" className="form-label">User Name</Form.Label>
         <Form.Control
           type="text"
           id="su_username"
           value={username}
           onChange={(e) => setUsername(e.target.value)}
+          placeholder="UserName"
         />
-        <Form.Label htmlFor="su_pwd">Password</Form.Label>
+        <Form.Label htmlFor="su_pwd" className="form-label mt-4">Password</Form.Label>
         <Form.Control
           type="password"
           id="su_pwd"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
         />
         <Button
           variant="primary"
-          className="w-100 mt-4 mb-4"
+          className="w-100 mt-4 mb-4 auth-btn"
           size="lg"
-          style={{
-            height: "80px",
-          }}
           onClick={doSignIn}
         >
           Sign In
         </Button>
+
+        <label className="text-black">You don't have account, please <Link to="/sign-up" className="w-100 text-center">Sign Up</Link></label>
       </div>
     </div>
   );
--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -18,43 +18,49 @@ const SignUp = () => {
   };
 
   return (
-    <div className="d-flex justify-content-center align-items-center w-100">
+    <div className="d-flex justify-content-center align-items-center w-100 auth-panel">
       <div className="sign-up">
-        <Form.Label htmlFor="su_username">User Name</Form.Label>
+        <h1 className="__auth-title">Sign Up</h1>
+
+        <hr />
+
+        <Form.Label htmlFor="su_username" className="form-label">User Name</Form.Label>
         <Form.Control
           type="text"
           id="su_username"
           value={username}
           onChange={(e) => setUsername(e.target.value)}
+          className="form-input"
+          placeholder="Jackson"
         />
-        <Form.Label htmlFor="su_email">E-mail Address</Form.Label>
+        <Form.Label htmlFor="su_email" className="mt-4">Email Address</Form.Label>
         <Form.Control
           type="text"
           id="su_email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
+          placeholder="face@bit.com"
         />
-        <Form.Label htmlFor="su_pwd">Password</Form.Label>
+        <Form.Label htmlFor="su_pwd" className="mt-4">Password</Form.Label>
         <Form.Control
           type="password"
           id="su_pwd"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
+          placeholder="******"
         />
-        <Form.Label htmlFor="su_pwd_confirm">Password Confirm</Form.Label>
+        <Form.Label htmlFor="su_pwd_confirm" className="mt-4">Password Confirm</Form.Label>
         <Form.Control
           type="password"
           id="su_pwd_confirm"
           value={passwordConfirm}
           onChange={(e) => setPasswordConfirm(e.target.value)}
+          placeholder="******"
         />
         <Button
           variant="primary"
-          className="w-100 mt-4 mb-4"
+          className="w-100 mt-4 mb-4 auth-btn"
           size="lg"
-          style={{
-            height: "80px",
-          }}
           onClick={doSignUp}
         >
           Sign Up
