1d11567c8ddee7e3fb2cf593ce36b3d73f3e0cd2
V <narutoninja342@gmail.com>
2024-06-13 11:42:06 +0800

    Update sign in

--- src/pages/signin/index.tsx
+++ src/pages/signin/index.tsx
@@ -1,13 +1,22 @@
 import axios from "axios";
 import { useState } from "react";
-import { Button, Form } from "react-bootstrap";
+import { useForm } from 'react-hook-form'
+import { Button, Form, FormGroup } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 const SignIn = () => {
+  const { register, handleSubmit, reset, formState } = useForm();
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
+  const [authResult, setAuthResult] = useState(true);
+
   const doSignIn = async () => {
+    if(username.length === 0 || password.length ===0) {
+      setValidated(true);
+      return;
+    }
+
     axios
       .post("http://192.168.6.244:4000/user/login", {
         username,
@@ -19,43 +28,58 @@ const SignIn = () => {
         window.location.href = "/home";
       })
       .catch((error) => {
+        setAuthResult(false);
         console.log(error);
       });
   };
+
+  const [validated, setValidated] = useState(false);
+
 
   return (
     <div className="d-flex justify-content-center align-items-center w-100 auth-panel">
       <div className="sign-up">
         <h1 className="__auth-title">Sign In</h1>
-
+        
         <hr />
 
-        <Form.Label htmlFor="su_username" className="form-label">User Name</Form.Label>
-        <Form.Control
-          type="text"
-          id="su_username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          placeholder="UserName"
-        />
-        <Form.Label htmlFor="su_pwd" className="form-label mt-4">Password</Form.Label>
-        <Form.Control
-          type="password"
-          id="su_pwd"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="Password"
-        />
-        <Button
-          variant="primary"
-          className="w-100 mt-4 mb-4 auth-btn"
-          size="lg"
-          onClick={doSignIn}
-        >
-          Sign In
-        </Button>
+        <Form noValidate validated={validated} onSubmit={handleSubmit(doSignIn)}>
+          <FormGroup>
+            <Form.Control.Feedback type="invalid" className={`${authResult ? 'd-none' : 'd-block'}`}>Credentials is incorrect.</Form.Control.Feedback>
+            <Form.Label htmlFor="su_username" className="form-label">User Name</Form.Label>
+            <Form.Control
+              required
+              type="text"
+              id="su_username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              placeholder="User Name"
+            />
+            <Form.Control.Feedback type="invalid">Please fill User Name.</Form.Control.Feedback>
+          </FormGroup>
+          <FormGroup>
+            <Form.Label htmlFor="su_pwd" className="form-label mt-4">Password</Form.Label>
+            <Form.Control
+              required
+              type="password"
+              id="su_pwd"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Password"
+            />
+            <Form.Control.Feedback type="invalid">Please fill Password.</Form.Control.Feedback>
+          </FormGroup>
+          <Button
+            variant="primary"
+            className="w-100 mt-4 mb-4 auth-btn"
+            size="lg"
+            type="submit"
+          >
+            Sign In
+          </Button>
 
-        <label className="text-black">You don't have account, please <Link to="/sign-up" className="w-100 text-center">Sign Up</Link></label>
+          <Form.Text className="text-black">You don't have account, please <Link to="/sign-up" className="w-100 text-center">Sign Up</Link></Form.Text >
+        </Form>
       </div>
     </div>
   );
--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,6 +1,7 @@
-import { Button, Form } from "react-bootstrap";
+import { Button, Form, FormGroup } from "react-bootstrap";
 import axios from "axios";
 import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 const SignUp = () => {
   const [username, setUsername] = useState("");
@@ -8,14 +9,38 @@ const SignUp = () => {
   const [password, setPassword] = useState("");
   const [passwordConfirm, setPasswordConfirm] = useState("");
 
+  const { register, handleSubmit, reset, formState } = useForm();
+
   const doSignUp = async () => {
-    if (password !== passwordConfirm) return;
+    if(username.length === 0 || password.length === 0 || email.length === 0 || passwordConfirm.length === 0) {
+      setValidated(true);
+      return;
+    }
+
+    if (password !== passwordConfirm) {
+      setValidated(true);
+      return;
+    }
+
     axios.post("http://192.168.6.244:4000/user/register", {
       username,
       password,
       email,
     });
   };
+
+  const [validated, setValidated] = useState(false);
+
+  // const handleSubmit = (event:any) => {
+  //   const form = event.currentTarget;
+  //   if (form.checkValidity() === false) {
+  //     event.preventDefault();
+  //     event.stopPropagation();
+  //     setValidated(true);
+  //   } else {
+  //     doSignUp();
+  //   }
+  // };
 
   return (
     <div className="d-flex justify-content-center align-items-center w-100 auth-panel">
@@ -24,47 +49,70 @@ const SignUp = () => {
 
         <hr />
 
-        <Form.Label htmlFor="su_username" className="form-label">User Name</Form.Label>
-        <Form.Control
-          type="text"
-          id="su_username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          className="form-input"
-          placeholder="Jackson"
-        />
-        <Form.Label htmlFor="su_email" className="mt-4">Email Address</Form.Label>
-        <Form.Control
-          type="text"
-          id="su_email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="face@bit.com"
-        />
-        <Form.Label htmlFor="su_pwd" className="mt-4">Password</Form.Label>
-        <Form.Control
-          type="password"
-          id="su_pwd"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="******"
-        />
-        <Form.Label htmlFor="su_pwd_confirm" className="mt-4">Password Confirm</Form.Label>
-        <Form.Control
-          type="password"
-          id="su_pwd_confirm"
-          value={passwordConfirm}
-          onChange={(e) => setPasswordConfirm(e.target.value)}
-          placeholder="******"
-        />
+        <Form noValidate validated={validated} onSubmit={handleSubmit(doSignUp)}>
+          <FormGroup>
+            <Form.Label htmlFor="su_username" className="form-label">User Name</Form.Label>
+            <Form.Control
+              required
+              type="text"
+              id="su_username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              className="form-input"
+              placeholder="Jackson"
+            />
+            <Form.Control.Feedback type="invalid">Please fill User Name.</Form.Control.Feedback>
+          </FormGroup>
+
+          <FormGroup>
+            <Form.Label htmlFor="su_email" className="mt-4">Email Address</Form.Label>
+            <Form.Control
+              required
+              type="email"
+              id="su_email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="face@bit.com"
+            />
+            <Form.Control.Feedback type="invalid">Please fill Email Address.</Form.Control.Feedback>
+          </FormGroup>
+
+        <FormGroup>
+          <Form.Label htmlFor="su_pwd" className="mt-4">Password</Form.Label>
+          <Form.Control
+            required
+            type="password"
+            id="su_pwd"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="******"
+          />
+          <Form.Control.Feedback type="invalid">Please fill Password.</Form.Control.Feedback>
+          {/* <Form.Control.Feedback type="invalid">Don't match password and confirm.</Form.Control.Feedback> */}
+        </FormGroup>
+
+        <FormGroup>
+          <Form.Label htmlFor="su_pwd_confirm" className="mt-4">Password Confirm</Form.Label>
+          <Form.Control
+            required
+            type="password"
+            id="su_pwd_confirm"
+            value={passwordConfirm}
+            onChange={(e) => setPasswordConfirm(e.target.value)}
+            placeholder="******"
+          />
+          <Form.Control.Feedback type="invalid">Please fill Password Confirm.</Form.Control.Feedback>
+        </FormGroup>
+
         <Button
           variant="primary"
           className="w-100 mt-4 mb-4 auth-btn"
           size="lg"
-          onClick={doSignUp}
+          type="submit"
         >
           Sign Up
         </Button>
+        </Form>
       </div>
     </div>
   );
